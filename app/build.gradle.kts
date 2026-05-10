ab9485cf3504cc47e1ef2ee98fdf7a889ee3a41b
tharindu899 <prabath99t@gmail.com>
2026-05-10 18:57:57 +0530

    2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk        = 26
         targetSdk     = 35
         versionCode   = 1
-        versionName   = "2.0.0"
+        versionName   = "2.0.4"
 
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"YOUR_WEB_CLIENT_ID_HERE.apps.googleusercontent.com\"")
     }
